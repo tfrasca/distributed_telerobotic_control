18d06826de4d59bcd4c5dcdf7f5d5c9b6ec5cd48
tfrasca <tmfrasca@localhost.localdomain>
2020-12-08 11:52:47 -0500

    changed processing of serial input to robot controller, assumes form (j0 j1 j2)

--- RobotControl/RobotControl.ino
+++ RobotControl/RobotControl.ino
@@ -7,35 +7,50 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  bool shouldRead = true;
-  int index = 0;
   bool shouldMove = false;
-  while (shouldRead) { // wait until 'end' is received 
-    if (Serial.available() > 0) {
-      String val = Serial.readStringUntil('\n');
-      if (val.equals("end")) {
-        shouldRead = false;
-        shouldMove = true;
+  if (Serial.available() > 0) {
+    String val = Serial.readStringUntil('\n');
+    int jointAngleStart = 0; // joint angle 0 starts at 0th character
+    int jointAngleEnd; // joint angle 0 ends at first instance of " "
+    // assuming properly formed input (j0 j1 j2 ... jn)
+    for (int index = 0; index < numJoints; index++) {
+      val = val.substring(jointAngleStart);
+      jointAngleEnd = val.indexOf(" "); // get substring from starting point to end, then find index of next " "
+      if (jointAngleEnd != -1) { // if space is found then get value
+        jointValues[index] = val.substring(0,jointAngleEnd).toInt(); // get value of joint angle
       } else {
-        if (index == numJoints) { // cannot accept more than numJoints joint positions
-          shouldRead = false;
-          shouldMove = false;
-        } else {
-          jointValues[index++] = val.toInt();
-        }
+        jointValues[index] = val.toInt();
       }
+      jointAngleStart = jointAngleEnd+1;
+
+      //if not assuming properly formed then need following will handle input that has too many values
+      //  but need to take into account if the input doesn't have enough values
+      //if (index == (numJoints - 1)) {
+      //  val = val.substring(jointAngleEnd);
+      //  if (val.equals(" ") || val.length() == 0) {
+      //    shouldMove = true;
+      //  }
+      //}
     }
-  }
-  if (shouldMove) {
-    moveJoints(jointValues);
+
+    //if (shouldMove) {
+      Serial.println("should move");
+      moveJoints(jointValues);
+    //}
   }
 }
 
-void moveJoints(int *jointValues){
-  Serial.println(jointValues[0]);
-  Serial.println(jointValues[1]);
-  Serial.println(jointValues[2]);
-  //dxlSetGoalPosition(1, jointValues[0]);
-  //dxlSetGoalPosition(2, jointValues[1]);
-  //dxlSetGoalPosition(3, jointValues[2]);
+void moveJoints(int *jointAngles){
+  for (int index =0; index < numJoints; index++) {
+    printJointValue(index+1, jointAngles[index]);
+    //dxlSetGoalPosition(index+1, jointAngles[index]);
+  }
+}
+
+void printJointValue(int jointIndex, int angle) {
+  Serial.print("j");
+  Serial.print(jointIndex);
+  Serial.print(" ");
+  Serial.print(angle);
+  Serial.println(" ");
 }
