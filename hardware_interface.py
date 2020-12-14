a6de9746bb72513798545aa7f2b8832288ba9327
tfrasca <tyfrasc15@gmail.com>
2020-12-14 18:12:49 -0500

    added subscriber for mode

--- hardware_interface.py
+++ hardware_interface.py
@@ -1,24 +1,37 @@
-import serial
-import time
 from pubnub import pubnub
-from threading import Thread
+from pubnub.pubnub import SubscribeCallback
 import pubnub_config
+import serial
+from threading import Thread
+import time
 
-#COMPORT = "/dev/ttyUSB0"
-COMPORT = "COM3"
+COMPORT = "/dev/ttyUSB1"
+#COMPORT = "COM3"
 BAUDRATE = 9600
 JOINT = "j0"
-joint_angle = 0
-mode = 0
 
-should_read_serial = True
-angle_delta_threshold = 10
 
+class Listener(SubscribeCallback):
+  def __init__(self, hardware_controller):
+    super().__init__()
+    self.hardware_controller = hardware_controller
+
+  def message(self, pn, message):
+    print(message.message)
+    if message.channel == "mode":
+      self.hardware_controller.update_mode(message.message)
 
 class HardwareController:
     def __init__(self, pub_nub):
         self.pub_nub = pub_nub
         self.should_read_serial = True
+        self.current_mode = 0
+        self.joint_angle = 0
+        self.old_joint_angle = 0
+        self.mode = 0
+        self.old_mode = 0
+        self.angle_delta_threshold = 5
+        self.ser = serial.Serial(port=COMPORT, baudrate=BAUDRATE)
 
     def run(self):
         t1 = Thread(target=self.process_serial)
@@ -28,42 +41,45 @@ class HardwareController:
 
     def update_pubnub(self):
         try:
-            old_joint_angle = 0
-            old_mode = 0
             while True:
-                print (old_mode, mode)
-                if old_mode != mode:
-                    self.pub_nub.publish().channel("mode").message(mode).pn_async(self.publisher_callback)
-                    old_mode = mode
-                if old_mode == 1:
-                    if abs(joint_angle - old_joint_angle) > angle_delta_threshold:
-                        self.pub_nub.publish().channel(JOINT).message(joint_angle).pn_async(self.publisher_callback)
-                        old_joint_angle = joint_angle
+                print (self.old_mode, self.mode)
+                if self.old_mode != self.mode:
+                    self.pub_nub.publish().channel("mode").message(self.mode).pn_async(self.publisher_callback)
+                    self.old_mode = self.mode
+                if self.old_mode == 1:
+                    if abs(self.joint_angle - self.old_joint_angle) > self.angle_delta_threshold:
+                        self.pub_nub.publish().channel(JOINT).message(self.joint_angle).pn_async(self.publisher_callback)
+                        self.old_joint_angle = self.joint_angle
                 time.sleep(1)
         except (KeyboardInterrupt):
             self.should_read_serial = False
 
     def process_serial(self):
-        global joint_angle, mode
-        ser = serial.Serial(port=COMPORT, baudrate=BAUDRATE)
         while self.should_read_serial:
-            line = ser.readline().decode().strip()
+            line = self.ser.readline().decode().strip()
             vals = line.split(" ")
             if vals[0] == "j":
-                joint_angle = int(vals[1])
+                self.joint_angle = int(vals[1])
             elif vals[0] == "m":
-                mode = int(vals[1])
+                self.mode = int(vals[1])
         ser.close()
 
     def publisher_callback(self, envelope, status):
         if status.is_error():
             print("error", status.status_code)
 
+    def update_mode(self, mode):
+      if self.old_mode != mode:
+          self.old_mode = mode
+          self.ser.write(str(mode).encode())
 
 if __name__ =="__main__":
     uuid = "tmfrasca"
     pn = pubnub_config.config(uuid)
     controller = HardwareController(pn)
+    listener = Listener(controller)
+    pn.add_listener(listener)
+    pn.subscribe().channels(["mode"]).execute()
     ts = Thread(target=controller.run())
     ts.start()
 
